091cbc257bef23f0d33b65bf5d531599396d7502
NicoBurno <nicoburno@gmail.com>
2017-06-07 11:04:13 +0000

    Init plugin

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import * as angular from 'angular';
+import CryptoPro from 'crypto-pro';
+
+class CryptoProService {
+	constructor($q) {
+		this.call = (...args) => {
+			return $q((resolve, reject) => {
+				CryptoPro.call(...args).then(resolve, reject);
+			});
+		}
+	}
+}
+CryptoProService.$inject = ['$q'];
+
+const module = angular
+	.module("angular-crypto-pro", [])
+	.factory("CryptoPro", CryptoProService);
+
+export default module.name;
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProduction = nodeEnv === 'production';
+
+const srcPath = path.join(__dirname, './src');
+const distPath = path.join(__dirname, './dist');
+
+const rules = [
+	{
+		test: /\.js$/,
+		loader: 'babel-loader',
+		exclude: /node_modules/
+	}
+];
+
+const plugins = [];
+
+if (isProduction) {
+	plugins.push(
+		new webpack.optimize.UglifyJsPlugin({
+			sourceMap: true
+		})
+	);
+}
+
+var config = module.exports = {
+    context: srcPath,
+    entry: './index.js',
+    output: {
+        path: distPath,
+        filename: 'angular-crypto-pro.js',
+        library: "angular-crypto-pro",
+        libraryTarget: "umd",
+        umdNamedDefine: true
+    },
+    devtool: isProduction ? 'source-map' : 'eval',
+    watch: true,
+    module: {
+    	rules
+    },
+    plugins,
+    externals: [
+    	"angular",
+    	"crypto-pro"
+    ]
+};
